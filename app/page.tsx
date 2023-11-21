c6073d2d4e53a4448256c642b6b0d54c7f4603d4
jonnyabrams <jonnyabrams@hotmail.com>
2023-11-21 01:43:39 +0000

    chore: initial routes setup

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="text-red-900 text-2xl">Hello world</h1>
-      <h2>Hello world</h2>
-      <h3>Hello world</h3>
-    </main>
-  );
-}
