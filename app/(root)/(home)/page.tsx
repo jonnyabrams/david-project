a79eab82fbf9142a7aa966841516200db6785d8a
jonnyabrams <jonnyabrams@hotmail.com>
2023-11-25 18:09:28 +0000

    feat: initial Navbar design

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
-
 const Home = () => {
-  return <UserButton afterSignOutUrl="/" />;
+  return <div></div>;
 };
 
 export default Home;
---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex flex-1 items-center gap-2">
+        <Image
+          src="/assets/images/me.jpeg"
+          width={26}
+          height={26}
+          alt="Share Our Best"
+        />
+
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          ShareOur<span className="text-primary-500">Best</span>
+        </p>
+      </Link>
+      GlobalSearch
+      <div className="flex-between gap-5">
+        Theme
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+
+        MobileNav
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
