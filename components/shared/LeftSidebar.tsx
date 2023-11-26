45b4f16eeb54737685718b7ab7f28084fb0c1988
jonnyabrams <jonnyabrams@hotmail.com>
2023-11-26 11:13:03 +0000

    feat: add login/signup to left sidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,8 +3,10 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import { SignedOut } from "@clerk/nextjs";
 
 import { sidebarLinks } from "@/constants";
+import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
@@ -40,6 +42,22 @@ const LeftSidebar = () => {
           );
         })}
       </div>
+
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <span className="primary-text-gradient">Log In</span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
     </section>
   );
 };
