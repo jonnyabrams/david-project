1e9a4fac58e44b34544bbadfb96466439f94b2a3
jonnyabrams <jonnyabrams@hotmail.com>
2023-11-26 14:20:53 +0000

    feat: homepage local searchbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Posts</h1>
+
+        <Link href="/create-post" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Create a Post
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for posts..."
+          otherClasses="flex-1"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Home;
