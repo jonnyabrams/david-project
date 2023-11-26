20d67b85d5619f7e93baacf442be1a0bf9228285
jonnyabrams <jonnyabrams@hotmail.com>
2023-11-26 11:01:51 +0000

    feat: LeftSidebar initial design

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+import { sidebarLinks } from "@/constants";
+
+const LeftSidebar = () => {
+  const pathname = usePathname();
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+
+          return (
+            <Link
+              href={item.route}
+              key={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default LeftSidebar;
