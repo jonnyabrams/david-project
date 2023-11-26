706ee87f59b5d0a28983ebdebb07cd3528b4d883
jonnyabrams <jonnyabrams@hotmail.com>
2023-11-26 11:23:24 +0000

    feat: collapsed leftsidebar with icons only

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -35,7 +35,11 @@ const LeftSidebar = () => {
                 height={20}
                 className={`${isActive ? "" : "invert-colors"}`}
               />
-              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+              <p
+                className={`${
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
+              >
                 {item.label}
               </p>
             </Link>
@@ -47,13 +51,31 @@ const LeftSidebar = () => {
         <div className="flex flex-col gap-3">
           <Link href="/sign-in">
             <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-              <span className="primary-text-gradient">Log In</span>
+              <Image
+                src="/assets/icons/account.svg"
+                alt="login"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log In
+              </span>
             </Button>
           </Link>
 
           <Link href="/sign-up">
             <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-              Sign Up
+            <Image
+                src="/assets/icons/sign-up.svg"
+                alt="signup"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">
+                Sign Up
+              </span>
             </Button>
           </Link>
         </div>
