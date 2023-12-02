dbbf6e2c522b42c1c0012e507bf525a1b48e005a
jonnyabrams <jonnyabrams@hotmail.com>
2023-12-02 10:22:19 +0000

    chore: add trusts & specialties constants

---
+++ constants/specialtyList.ts
@@ -0,0 +1,22 @@
+export const specialtyList = [
+  "Specialty 1",
+  "Specialty 2",
+  "Specialty 3",
+  "Specialty 4",
+  "Specialty 5",
+  "Specialty 6",
+];
+
+export const specialty1SubsList = [
+  "Subspecialty A",
+  "Subspecialty B",
+  "Subspecialty C",
+  "Subspecialty D",
+]
+
+export const specialty3SubsList = [
+  "Subspecialty E",
+  "Subspecialty F",
+  "Subspecialty G",
+  "Subspecialty H",
+]
