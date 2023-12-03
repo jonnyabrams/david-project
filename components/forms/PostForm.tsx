9ce9ca2d890e52b30d4bb82b9a0f03a4caf72882
jonnyabrams <jonnyabrams@hotmail.com>
2023-12-03 14:43:46 +0000

    feat: add/remove tags

--- components/forms/PostForm.tsx
+++ components/forms/PostForm.tsx
@@ -18,6 +18,9 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
 import { PostSchema } from "@/lib/validations";
+import { KeyboardEvent } from "react";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
 
 const PostForm = () => {
   const form = useForm<z.infer<typeof PostSchema>>({
@@ -29,11 +32,44 @@ const PostForm = () => {
     },
   });
 
-  // 2. Define a submit handler.
   const onSubmit = (values: z.infer<typeof PostSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
+  };
+
+  const handleInputKeyDown = (
+    e: KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 30) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be fewer than 30 characters",
+          });
+        }
+      }
+
+      // check the tag doesn't already exist in the field - avoid duplication
+      if (!field.value.includes(tagValue as never)) {
+        form.setValue("tags", [...field.value, tagValue]);
+        tagInput.value = "";
+        form.clearErrors("tags");
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const filteredTags = field.value.filter((t: string) => t !== tag);
+
+    form.setValue("tags", filteredTags);
   };
 
   return (
@@ -77,6 +113,7 @@ const PostForm = () => {
                 <Textarea
                   placeholder="Share your best..."
                   className="no-focus paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 min-h-[126px] border"
+                  {...field}
                 />
               </FormControl>
               <FormMessage className="text-red-500" />
@@ -93,11 +130,33 @@ const PostForm = () => {
                 Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  placeholder="Add tags..."
-                  className="no-focus paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  {...field}
-                />
+                <>
+                  <Input
+                    placeholder="Add tags..."
+                    className="no-focus paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="Close icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 relevant tags.
