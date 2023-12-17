ed34101ab0210f210fca8610223527daef31b754
jonnyabrams <jonnyabrams@hotmail.com>
2023-12-17 11:58:03 +0000

    fix: unrequire various user attributes

--- models/user.model.ts
+++ models/user.model.ts
@@ -2,20 +2,20 @@ import { Schema, models, model, Document } from "mongoose";
 
 export interface IUser extends Document {
   clerkId: string;
-  salutation: string;
-  firstName: string;
-  surname: string;
+  salutation?: string;
+  firstName?: string;
+  surname?: string;
   email: string;
   password?: string;
   bio?: string;
   trust?: string;
   specialty?: string;
   subSpecialty?: string;
-  picture: string;
+  picture?: string;
   location?: string;
   website?: string;
   reputation?: number;
-  savedPosts: Schema.Types.ObjectId[];
+  savedPosts?: Schema.Types.ObjectId[];
   createdAt: Date;
   updatedAt: Date;
 }
@@ -23,13 +23,13 @@ export interface IUser extends Document {
 const UserSchema = new Schema(
   {
     clerkId: { type: String, required: true },
-    salutation: { type: String, required: true },
-    firstName: { type: String, required: true },
-    surname: { type: String, required: true },
+    salutation: { type: String },
+    firstName: { type: String },
+    surname: { type: String },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
     bio: { type: String },
-    picture: { type: String, required: true },
+    picture: { type: String },
     location: { type: String },
     website: { type: String },
     reputation: { type: Number, default: 0 },
