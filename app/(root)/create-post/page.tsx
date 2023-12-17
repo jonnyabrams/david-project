5af588d2ceb514c2cb68ee956dc55e81896f7774
jonnyabrams <jonnyabrams@hotmail.com>
2023-12-17 15:07:55 +0000

    feat: fetch db user for create-post

--- app/(root)/create-post/page.tsx
+++ app/(root)/create-post/page.tsx
@@ -1,17 +1,18 @@
 import PostForm from "@/components/forms/PostForm";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
 import { getUserById } from "@/lib/actions/user.action";
 
 const CreatePost = async () => {
-  // const { userId } = auth();
-  const userId = "CL123"
+  const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
 
   const dbUser = await getUserById({ userId });
 
-  console.log(dbUser)
+  console.log("HEY", dbUser)
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900 ">Create a Post</h1>
