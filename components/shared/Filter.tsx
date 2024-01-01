eb4b09fe4b9edb3c74d3c872f8a40c63fb9340bb
jonnyabrams <jonnyabrams@hotmail.com>
2024-01-01 12:43:33 +0000

    fix: filter styles

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -43,16 +43,20 @@ const Filter = ({ filters, otherClasses, containerClasses }: FilterProps) => {
         defaultValue={paramFilter || undefined}
       >
         <SelectTrigger
-          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5 focus:outline-none`}
         >
           <div className="line-clamp-1 flex-1 text-left">
             <SelectValue placeholder="Select a Filter" />
           </div>
         </SelectTrigger>
-        <SelectContent>
+        <SelectContent className="text-dark500_light700 small-regular border-none bg-light-900 focus:outline-none active:outline-none dark:bg-dark-300">
           <SelectGroup>
             {filters.map((filter) => (
-              <SelectItem key={filter.value} value={filter.value}>
+              <SelectItem
+                key={filter.value}
+                value={filter.value}
+                className="cursor-pointer focus:bg-light-800 dark:focus:bg-dark-400"
+              >
                 {filter.name}
               </SelectItem>
             ))}
