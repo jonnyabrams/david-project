91f9b6f053c2776da8fb2df1815fd3c53d6501cb
jonnyabrams <jonnyabrams@hotmail.com>
2024-01-28 12:19:24 -0500

    fix: stop liked icon shrinking on dark mode

--- components/shared/Likes.tsx
+++ components/shared/Likes.tsx
@@ -79,7 +79,7 @@ const Likes = ({
               fill="#9898ee"
               size={20}
               onClick={handleLike}
-              className="cursor-pointer"
+              className="cursor-pointer text-[#9898ee]"
             />
           ) : (
             <ThumbsUp
